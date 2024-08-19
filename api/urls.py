29c17438ec5cb1f85803a606869105c9c9fd2959
kcabhinav <kcabhinav42@gmail.com>
2024-08-19 19:43:25 +0530

    add something

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import PostView, PostListView, AuthorListView, AuthorView
+from .views import PostView, PostListView, AuthorListView, AuthorView, api_root
 urlpatterns = [
+    path('', api_root, name='api_root'),
     path('post/', PostListView.as_view(), name = 'post_list_view'),
     path('post/<int:pk>', PostView.as_view(), name = 'post_view'),
     path('author/', AuthorListView.as_view(), name = 'author_list_view'),
     path('author/<int:pk>', AuthorView.as_view(), name = 'author_view'),
-
 ]
--- api/views.py
+++ api/views.py
@@ -1,26 +1,39 @@
+from re import search
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.mixins import DestroyModelMixin, ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
 from .serializers import PostSerializer, AuthorSerializer
 
 from .models import Post, Author
 # Create your views here.
 
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'posts': reverse('post_list_view', request=request, format=format),
+        'authors': reverse('author_list_view', request=request, format=format)
+    })
+
 class PostListView(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class AuthorListView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
 
 class AuthorView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
